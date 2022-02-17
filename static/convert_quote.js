e2875891a9fc40149b53fcb532abcda49dbd7d02
mdwiltfong <wiltfong.michael1@gmail.com>
2022-02-17 08:41:08 -0500

    quote is being sent to front end

--- static/convert_quote.js
+++ static/convert_quote.js
@@ -1,4 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   student_name = document.getElementById("student_name");
   student_email = document.getElementById("student_email");
+  form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e);
+    fetchQuote(e.target[2].value, e.target[1].value)
+      .then((resp) => {
+        console.log(resp);
+      })
+      .then(renderQuote());
+    console.log("submit");
+  });
 });
+
+async function fetchQuote(email, name) {
+  resp = await fetch("/quotes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      student_email: email,
+      student_name: name,
+    }),
+  });
+  return resp.json();
+}
+
+function renderQuote(quote) {}
