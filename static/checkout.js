a60248ef139e725d7a5d0e7c87efe7f2f2287feb
mdwiltfong <wiltfong.michael1@gmail.com>
2022-02-26 20:02:04 -0500

    changed script file to checkout

---
+++ static/checkout.js
@@ -0,0 +1,97 @@
+let stripe, clientSecret, elements, cardElement;
+
+window.addEventListener("DOMContentLoaded", () => {
+  getConfig().then(() => {
+    const appearance = {
+      iconStyle: "solid",
+      style: {
+        base: {
+          iconColor: "#fff",
+          color: "#fff",
+          fontWeight: 400,
+          fontFamily: "Helvetica Neue, Helvetica, Arial, sans-serif",
+          fontSize: "16px",
+          fontSmoothing: "antialiased",
+
+          "::placeholder": {
+            color: "#BFAEF6",
+          },
+          ":-webkit-autofill": {
+            color: "white",
+          },
+        },
+        invalid: {
+          iconColor: "#FFC7EE",
+          color: "#FFC7EE",
+        },
+      },
+    };
+
+    elements = stripe.elements({ clientSecret, appearance });
+    cardElement = elements.create("card", appearance);
+    cardElement.mount("#payment-element");
+  });
+});
+
+async function getConfig() {
+  const details = await fetch("/config");
+  const { account_id, client_secret } = await details.json();
+  stripe = Stripe(
+    "pk_test_51KMM12DBo40mpW7dSao4A4hiqc7G349eWgYu5racVc0xhtUJColbp4U7LJd8WqBx4fdEdxbbQTEmZruFB40oCso300XBtrIET5",
+    { stripeAccount: account_id }
+  );
+  clientSecret = client_secret;
+}
+
+// helper method for displaying a status message.
+const setMessage = (message) => {
+  const messageDiv = document.querySelector("#messages");
+  if (message.error) {
+    messageDiv.className += " w-25 alert alert-danger";
+    messageDiv.innerHTML += message.error.message;
+  } else {
+    messageDiv.className = "w-25 alert alert-success";
+    messageDiv.innerHTML += message;
+  }
+};
+const form = document.querySelector("#payment-form");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const phoneInput = document.getElementById("phone");
+  const addressInput = document.getElementById("address");
+  const cityInput = document.getElementById("city");
+  const stateInput = document.getElementById("state");
+  const zipInput = document.getElementById("zip");
+  // Create payment method and confirm payment intent.
+  stripe
+    .confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: cardElement,
+        billing_details: {
+          name: nameInput.value,
+          email: emailInput.value,
+          address: {
+            city: cityInput.value,
+            line1: addressInput.value,
+            postal_code: zipInput.value,
+            state: stateInput.value,
+          },
+          phone: phoneInput.value,
+        },
+      },
+      setup_future_usage: "off_session",
+    })
+    .then((result) => {
+      if (result.error) {
+        setMessage(result);
+      } else {
+        // Redirect the customer to their account page
+        console.log("--->", result);
+        setMessage("Success! Redirecting to your account.");
+        window.location.href = "/checkout_successful";
+      }
+    });
+});
--- static/script.js
+++ static/script.js
@@ -1,97 +1,20 @@
-let stripe, clientSecret, elements, cardElement;
+// Get the container element
+var supportedContent = document.getElementById("navbarSupportedContent");
 
-window.addEventListener("DOMContentLoaded", () => {
-  getConfig().then(() => {
-    const appearance = {
-      iconStyle: "solid",
-      style: {
-        base: {
-          iconColor: "#fff",
-          color: "#fff",
-          fontWeight: 400,
-          fontFamily: "Helvetica Neue, Helvetica, Arial, sans-serif",
-          fontSize: "16px",
-          fontSmoothing: "antialiased",
+// Get all buttons with class="btn" inside the container
+var btns = supportedContent.getElementsByTagName("li");
 
-          "::placeholder": {
-            color: "#BFAEF6",
-          },
-          ":-webkit-autofill": {
-            color: "white",
-          },
-        },
-        invalid: {
-          iconColor: "#FFC7EE",
-          color: "#FFC7EE",
-        },
-      },
-    };
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
 
-    elements = stripe.elements({ clientSecret, appearance });
-    cardElement = elements.create("card", appearance);
-    cardElement.mount("#payment-element");
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
   });
-});
-
-async function getConfig() {
-  const details = await fetch("/config");
-  const { account_id, client_secret } = await details.json();
-  stripe = Stripe(
-    "pk_test_51KMM12DBo40mpW7dSao4A4hiqc7G349eWgYu5racVc0xhtUJColbp4U7LJd8WqBx4fdEdxbbQTEmZruFB40oCso300XBtrIET5",
-    { stripeAccount: account_id }
-  );
-  clientSecret = client_secret;
 }
-
-// helper method for displaying a status message.
-const setMessage = (message) => {
-  const messageDiv = document.querySelector("#messages");
-  if (message.error) {
-    messageDiv.className += " w-25 alert alert-danger";
-    messageDiv.innerHTML += message.error.message;
-  } else {
-    messageDiv.className = "w-25 alert alert-success";
-    messageDiv.innerHTML += message;
-  }
-};
-const form = document.querySelector("#payment-form");
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const nameInput = document.getElementById("name");
-  const emailInput = document.getElementById("email");
-  const phoneInput = document.getElementById("phone");
-  const addressInput = document.getElementById("address");
-  const cityInput = document.getElementById("city");
-  const stateInput = document.getElementById("state");
-  const zipInput = document.getElementById("zip");
-  // Create payment method and confirm payment intent.
-  stripe
-    .confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: cardElement,
-        billing_details: {
-          name: nameInput.value,
-          email: emailInput.value,
-          address: {
-            city: cityInput.value,
-            line1: addressInput.value,
-            postal_code: zipInput.value,
-            state: stateInput.value,
-          },
-          phone: phoneInput.value,
-        },
-      },
-      setup_future_usage: "off_session",
-    })
-    .then((result) => {
-      if (result.error) {
-        setMessage(result);
-      } else {
-        // Redirect the customer to their account page
-        console.log("--->", result);
-        setMessage("Success! Redirecting to your account.");
-        window.location.href = "/checkout_successful";
-      }
-    });
-});
